56bbb786866d922d2988ea07ce5cbdcee9cc0878
Jorge-Centeno <gcent3215@gmail.com>
2022-04-04 19:11:00 -0700

    first_java_program

---
+++ Week1/Extra Practice/first_java_program/FirstProgram.java
@@ -0,0 +1,7 @@
+public class FirstProgram {
+    public static void main(String[] args) {
+    System.out.println("My name is Jorge Centeno");
+    System.out.println("I am 40 years old");
+    System.out.println("My hometown is El Centro, CA");
+}
+}
